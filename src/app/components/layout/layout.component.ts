ee2ebdb20089247e11fd24bc69c9ea9316d0812c
itaytzdaka <itaytzdaka1@gmail.com>
2021-10-01 12:16:09 +0300

    chat design changes

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -12,49 +12,49 @@ import { Subject } from 'rxjs';
 export class LayoutComponent implements OnInit ,AfterViewChecked {
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
-  public backgroundColor: string;
-  public status: string;
+  // public backgroundColor: string;
+  // public status: string;
   public time: string;
-  public timeLeft: string;
+  // public timeLeft: string;
   public chatContent: string;
   public name: string;
   public message: string;
-  public onTheWay: boolean;
+  // public onTheWay: boolean;
   public interval;
 
   constructor
     (private mySocketService: SocketService, private renderer: Renderer2) {
     this.chatContent = "";
-    this.status = "אין";
-    this.backgroundColor = "grey";
-    this.onTheWay = false;
+    // this.status = "אין";
+    // this.backgroundColor = "grey";
+    // this.onTheWay = false;
   }
 
   ngOnInit(): void {
 
     this.mySocketService.listen('msg-from-server').subscribe((data: string) => {
       this.chatContent = `${this.chatContent} <div class="div-message"> ${data} </div>`;
-      this.playMessae();
+      this.playMessage();
     });
 
-    this.mySocketService.listen('status-from-server').subscribe((data: string) => {
-      clearInterval(this.interval);
-      this.timeLeft = undefined;
-      this.time = undefined;
-      this.status = data;
-      this.updateBackgroundColor();
-    });
+    // this.mySocketService.listen('status-from-server').subscribe((data: string) => {
+    //   clearInterval(this.interval);
+    //   this.timeLeft = undefined;
+    //   this.time = undefined;
+    //   this.status = data;
+    //   this.updateBackgroundColor();
+    // });
 
-    this.mySocketService.listen('time-from-server').subscribe((data: string) => {
-      this.time = data;
-      clearInterval(this.interval);
-      this.setTimer();
-    });
+    // this.mySocketService.listen('time-from-server').subscribe((data: string) => {
+    //   this.time = data;
+    //   clearInterval(this.interval);
+    //   this.setTimer();
+    // });
 
-    this.mySocketService.listen('arrived-from-server').subscribe((data: string) => {
-      this.chatContent = `${this.chatContent} <div class="div-message"> ${data} </div>`;
-      this.playBabyShark();
-    });
+    // this.mySocketService.listen('arrived-from-server').subscribe((data: string) => {
+    //   this.chatContent = `${this.chatContent} <div class="div-message"> ${data} </div>`;
+    //   this.playBabyShark();
+    // });
 
   }
 
@@ -63,104 +63,104 @@ export class LayoutComponent implements OnInit ,AfterViewChecked {
     // this.messages.next(this.message);
     console.log("sendMsg");
     if(this.message){
-      this.mySocketService.emit("msg-from-client", `<p class="name" > ${this.name || ""}</p><p class="time"> ${new Date().toString().slice(16, 21)}</p><p class="message">${this.message}</p>`);
+      this.mySocketService.emit("msg-from-client", `<div><p class="name" > ${this.name || ""}</p><p class="message">${this.message}</p><p class="time"> ${new Date().toString().slice(16, 21)}</p></div>`);
       this.message = undefined;
     }
   }
 
 
-  statusChanged() {
-    this.mySocketService.emit("status-from-client", this.status);
-    this.updateBackgroundColor();
+  // statusChanged() {
+  //   this.mySocketService.emit("status-from-client", this.status);
+  //   this.updateBackgroundColor();
 
-  }
+  // }
 
-  updateBackgroundColor() {
-    switch (this.status) {
-      case "לא מגיעה":
-        this.backgroundColor = "rgba(255,0,0,0.8)";
-        this.onTheWay = false;
-        break;
+  // updateBackgroundColor() {
+  //   switch (this.status) {
+  //     case "לא מגיעה":
+  //       this.backgroundColor = "rgba(255,0,0,0.8)";
+  //       this.onTheWay = false;
+  //       break;
 
-      case "מתלבטת":
-        this.backgroundColor = "rgba(252,135,0,0.8)";
-        this.onTheWay = false;
-        break;
+  //     case "מתלבטת":
+  //       this.backgroundColor = "rgba(252,135,0,0.8)";
+  //       this.onTheWay = false;
+  //       break;
 
-      case "החלטתי לבוא":
-        this.backgroundColor = "rgba(0,203,23,0.8)";
-        this.onTheWay = false;
-        break;
+  //     case "החלטתי לבוא":
+  //       this.backgroundColor = "rgba(0,203,23,0.8)";
+  //       this.onTheWay = false;
+  //       break;
 
-      case "מתכוננת":
-        this.backgroundColor = "rgba(211,31,225,0.8)";
-        this.onTheWay = false;
-        break;
+  //     case "מתכוננת":
+  //       this.backgroundColor = "rgba(211,31,225,0.8)";
+  //       this.onTheWay = false;
+  //       break;
 
-      case "יצאתי":
-        this.backgroundColor = "rgba(11,0,255,0.8)";
-        this.onTheWay = true;
-        break;
+  //     case "יצאתי":
+  //       this.backgroundColor = "rgba(11,0,255,0.8)";
+  //       this.onTheWay = true;
+  //       break;
 
-      default:
-        this.backgroundColor = "grey";
-        this.onTheWay = false;
-    }
-  }
-
-
-  timeDeclared() {
-    this.mySocketService.emit("time-from-client", this.time);
-    this.setTimer();
-  }
-
-  setTimer() {
-
-    clearInterval(this.interval);
-    if (!this.time)
-      return;
-
-    let arrivedTime = new Date();
-    console.log(this.time.slice(0, 3));
-    console.log(this.time.slice(3, 0));
-    arrivedTime.setHours(+this.time.slice(0, 2));
-    arrivedTime.setMinutes(+this.time.slice(3, 6));
+  //     default:
+  //       this.backgroundColor = "grey";
+  //       this.onTheWay = false;
+  //   }
+  // }
 
 
-    // Set the date we're counting down to
-    var countDownDate = arrivedTime.getTime();
+  // timeDeclared() {
+  //   this.mySocketService.emit("time-from-client", this.time);
+  //   this.setTimer();
+  // }
 
-    // Update the count down every 1 second
-    this.interval = setInterval(() => {
+  // setTimer() {
 
-      // Get today's date and time
-      var now = new Date().getTime();
+  //   clearInterval(this.interval);
+  //   if (!this.time)
+  //     return;
 
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-
-      // Time calculations for days, hours, minutes and seconds
-      // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + hours * 60;
-      // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // Display the result in the element with id="demo"
-      this.timeLeft = minutes + " דקות";
-
-      // If the count down is finished, write some text
-      if (distance < 0) {
-        clearInterval(this.interval);
-        this.timeLeft = "0 דקות";
-      }
-    }, 1000);
-  }
+  //   let arrivedTime = new Date();
+  //   console.log(this.time.slice(0, 3));
+  //   console.log(this.time.slice(3, 0));
+  //   arrivedTime.setHours(+this.time.slice(0, 2));
+  //   arrivedTime.setMinutes(+this.time.slice(3, 6));
 
 
-  arrived() {
-    this.mySocketService.emit("arrived-from-client", "הגעתי!");
-    this.playBabyShark();
-  }
+  //   // Set the date we're counting down to
+  //   var countDownDate = arrivedTime.getTime();
+
+  //   // Update the count down every 1 second
+  //   this.interval = setInterval(() => {
+
+  //     // Get today's date and time
+  //     var now = new Date().getTime();
+
+  //     // Find the distance between now and the count down date
+  //     var distance = countDownDate - now;
+
+  //     // Time calculations for days, hours, minutes and seconds
+  //     // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + hours * 60;
+  //     // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //     // Display the result in the element with id="demo"
+  //     this.timeLeft = minutes + " דקות";
+
+  //     // If the count down is finished, write some text
+  //     if (distance < 0) {
+  //       clearInterval(this.interval);
+  //       this.timeLeft = "0 דקות";
+  //     }
+  //   }, 1000);
+  // }
+
+
+  // arrived() {
+  //   this.mySocketService.emit("arrived-from-client", "הגעתי!");
+  //   this.playBabyShark();
+  // }
 
 
 
@@ -184,17 +184,17 @@ export class LayoutComponent implements OnInit ,AfterViewChecked {
     } catch (err) { }
   }
 
-  playMessae(){
+  playMessage(){
     let audio = new Audio();
     audio.src = "../../../assets/audio/message.mp3";
     audio.load();
     audio.play();
   }
 
-  playBabyShark(){
-    let audio = new Audio();
-    audio.src = "../../../assets/audio/baby-shark.mp3";
-    audio.load();
-    audio.play();
-  }
+  // playBabyShark(){
+  //   let audio = new Audio();
+  //   audio.src = "../../../assets/audio/baby-shark.mp3";
+  //   audio.load();
+  //   audio.play();
+  // }
 }
